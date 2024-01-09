8f110649f01b015df1b1a7764801118969c4c733
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2024-01-09 22:31:43 +0200

    init repo

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log("Hello World!");
+});
+</script>
+
+<template>
+  <div class="bg-black text-white w-screen h-screen flex justify-center items-center">
+    <h1>Hello world!</h1>
+  </div>
+</template>
